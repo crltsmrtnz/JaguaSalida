f337c2c8494761897a807aaa6a1d0f20919b6a94
CrltsMrtnz <crltsmrtnz@gmail.com>
2020-03-30 21:20:10 -0400

    Deploy web

---
+++ js/animacion-nav.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $(".header").css({
+        "width": $(window).width() + "px"
+    });
+    $(".navbar").css({
+        "background-color": "#fff",
+        "-webkit-box-shadow": "0px 6px 4px 1px rgba(0, 0, 0, 0.07)",
+        "-moz-box-shadow": "0px 6px 4px 1px rgba(0, 0, 0, 0.07)",
+        "box-shadow": "0px 6px 4px 1px rgba(0, 0, 0, 0.07)"
+    });
+    $(".nav-link").css('color', '#1da5b7');
+    $('#menu').addClass("fixed");
+    // <![CDATA[
+   
+    // ]]>  
+})
